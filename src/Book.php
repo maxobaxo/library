828cb0b14627552ca8f36b18cd38bd571229ae2e
Max Scher and Nathan Stewart <max@>
2017-07-19 08:17:20 -0700

    create initial file structure with Book class skeleton

---
+++ src/Book.php
@@ -0,0 +1,58 @@
+<?php
+    class Book
+    {
+        private $title;
+        private $id;
+
+        function __construct($title, $id = null)
+        {
+            $this->title = $title;
+            $this->id = $id;
+        }
+
+        function getTitle()
+        {
+
+        }
+
+        function setTitle()
+        {
+
+        }
+
+        function getId()
+        {
+
+        }
+
+        function save()
+        {
+
+        }
+
+        static function getAll()
+        {
+
+        }
+
+        static function deleteAll()
+        {
+
+        }
+
+        static function find()
+        {
+
+        }
+
+        function update()
+        {
+
+        }
+
+        function delete()
+        {
+            
+        }
+    }
+?>
